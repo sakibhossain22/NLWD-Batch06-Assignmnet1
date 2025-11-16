989bdbd76aba7460d433740f62234f1d6cf0cf6f
Md Sakib Mia <sakib01181@gmail.com>
2025-11-16 21:50:22 +0600

    problem 1 formatValue Solved

---
+++ solution.ts
@@ -0,0 +1,9 @@
+const formatValue = (value: string | number | boolean) => {
+    if (typeof value === 'string') {
+        return value.toUpperCase()
+    } else if (typeof value === 'number') {
+        return value * 10
+    } else if (typeof value === 'boolean') {
+        return value === true ? false : true
+    }
+}
