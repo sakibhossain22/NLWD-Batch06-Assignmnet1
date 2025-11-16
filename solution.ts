df9a8f7192be3d3697570eaac2dbbc8aa9bbe525
Md Sakib Mia <sakib01181@gmail.com>
2025-11-16 21:52:01 +0600

    problem 3 getDetails from class solved

--- solution.ts
+++ solution.ts
@@ -18,3 +18,16 @@ const getLength = (value: string | number[]) => {
     }
 
 }
+
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}`
+    }
+}
+
