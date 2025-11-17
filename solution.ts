c45f729ea01aa75c78d5112ea9ffd2c66f76683e
Md Sakib Mia <sakib01181@gmail.com>
2025-11-17 20:58:00 +0600

    Comment And Extra Code removed

--- solution.ts
+++ solution.ts
@@ -146,10 +146,3 @@ const calculateTotalPrice = (products: TPrice[]): number => {
     }
     return 0
 };
-const products = [
-  { name: 'Pen', price: 10, quantity: 2 },
-  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products));
